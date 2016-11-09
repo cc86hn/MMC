faae3ae04f9eb95ee2d51309c64df948980bdd4a
masterX244 <masterX244@googlemail.com>
2016-11-09 21:16:44 +0100

    Refactor; CRC8 moved to shared class

--- Audio/src/main/java/org/cc86/MMC/modules/audio/drivers/technics/se540/ProtocolHandler.java
+++ Audio/src/main/java/org/cc86/MMC/modules/audio/drivers/technics/se540/ProtocolHandler.java
@@ -12,6 +12,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.cc86.MMC.API.CRC;
 import org.cc86.MMC.modules.audio.StereoControl;
 
 /**
@@ -35,25 +36,6 @@ public class ProtocolHandler
     private static final int CNT_BYTE = 0;
     private static final byte BASE_SIZE_MASK = 0x07;
     private static final int BASE_SIZE_LENGTH = 3;
-    private static final int[] CRC8_TABLE = new int[]
-        {0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
-	157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
-	35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
-	190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
-	70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
-	219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
-	101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
-	248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
-	140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
-	17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
-	175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
-	50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
-	202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
-	87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
-	233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
-	116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53};
-    private static final int CRC8_START = 0xff;
-    
     
     private static final int SRV_BYTE = 1;
     private static final int SRV_MASK = 0x07;
@@ -133,18 +115,7 @@ public class ProtocolHandler
     //private final StereoControl control;
     
     
-    private static byte crc(int oldcrc,int in)
-    {
-        if(oldcrc<0)
-        {
-            oldcrc=(oldcrc+256)&0xff;
-        }
-        if(in<0)
-        {
-            in=(in+256)&0xff;
-        }
-        return (byte)CRC8_TABLE[oldcrc^in];
-    }
+   
     public ProtocolHandler(/*StereoControl c*/)
     {
         //control=c;
@@ -182,11 +153,11 @@ public class ProtocolHandler
                 }
                 if(suspected_package.size()>size_first+1)
                 {
-                    int crc = CRC8_START;
+                    int crc = CRC.CRC8_START;
                     int realsize = size_first+2;
                     for(int i=0;i<realsize;i++)
                     {
-                        crc = crc((byte)crc,suspected_package.get(i));
+                        crc = CRC.crc8((byte)crc,suspected_package.get(i));
                     }
                     if(crc==0)//CRC mit gültigem CRC-wert soll immer auf 0 enden
                     {
@@ -290,10 +261,10 @@ public class ProtocolHandler
         {
             raw_packet.set(i+USRDATA_REQ_START_BYTE, userdata.get(i));
         }
-        int crc = CRC8_START;
+        int crc = CRC.CRC8_START;
         for(int i=0;i<packetlength;i++)
         {
-            crc = crc((byte)crc,raw_packet.get(i));
+            crc = CRC.crc8((byte)crc,raw_packet.get(i));
         }
         raw_packet.set(packetlength,(byte)crc);
         while(raw_packet.size()>packetlength+1)
@@ -359,10 +330,10 @@ public class ProtocolHandler
         raw_packet.add(SRV_BYTE,(byte)(raw_packet.get(SRV_BYTE)|(SRV_RET<<SRV_OFFSET)));
         int packetlength = USRDATA_RET_START_BYTE+userdata.size();
         raw_packet.addAll(USRDATA_RET_START_BYTE, userdata);
-        int crc = CRC8_START;
+        int crc = CRC.CRC8_START;
         for(int i=0;i<packetlength;i++)
         {
-            crc = crc((byte)crc,raw_packet.get(i));
+            crc = CRC.crc8((byte)crc,raw_packet.get(i));
         }
         raw_packet.add(packetlength,(byte)crc);
         DriverSe540.getDriver().sendDataViaUart(raw_packet.toArray(new Byte[0]));
